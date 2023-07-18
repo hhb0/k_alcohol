09ee062557514476f75be5c0f1291cd2ca592cea
hhb0 <131655783+hhb0@users.noreply.github.com>
2023-07-18 09:37:03 +0900

    Update k_trenditonal_drinks.py

--- k_trenditonal_drinks.py
+++ k_trenditonal_drinks.py
@@ -19,7 +19,7 @@ from openai.embeddings_utils import (
 import matplotlib.pyplot as plt
 import plotly.express as px
 from scipy import spatial
-from sklearn.decomposition import PCA
+from scikit-learn.decomposition import PCA
 
 @st.cache_resource
 def init_connection():
